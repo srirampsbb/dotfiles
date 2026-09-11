09977769fcf20574c0dfeece7691ca1d18273319
srirampsbb <srirampsbb@gmail.com>
2026-09-10 18:59:42 -0700

    fix old wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -19,7 +19,11 @@ config.default_cursor_style = 'SteadyBar'
 
 -- macOS Translucency & Styling
 config.window_background_opacity = 0.80
-config.inactive_pane_opacity = 0.80
+config.inactive_pane_hsb = {
+  hue = 1.0,
+  saturation = 1.0,
+  brightness = 0.8, -- adjust this value to control dimming (0.0 to 1.0)
+}
 config.macos_window_background_blur = 50
 -- config.color_scheme = "rose-pine-moon"
 config.color_scheme = 'Kanagawa (Gogh)'
